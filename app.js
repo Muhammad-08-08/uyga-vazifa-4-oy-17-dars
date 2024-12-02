778a2bbba8a234db5be9dd7f204e39d645d65ae5
muhammad <muhammadnuraliyev119@gmail.com>
2024-12-02 22:28:16 +0500

    muhammad

---
+++ app.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let hour = document.querySelector(".hour");
+  let minut = document.querySelector(".minute");
+
+  const darkMoon = document.querySelector(".dark-moon-box");
+  const container = document.querySelector(".container");
+  const blackWhite = document.querySelector(".black-white");
+  const card1 = document.querySelector(".card1");
+
+  darkMoon.addEventListener("click", () => {
+    blackWhite.classList.toggle("black-white-right");
+    container.classList.toggle("moon");
+    card1.classList.toggle("moon");
+  });
+
+  setInterval(() => {
+    let newDay = new Date();
+    hour.innerHTML = newDay.getHours().toString().padStart(2, "0");
+    minut.innerHTML = newDay.getMinutes().toString().padStart(2, "0");
+  }, 1000);
+
+  let day = document.querySelector(".day");
+  let month = document.querySelector(".month");
+  let kun = document.querySelector(".kun");
+
+  const weekDays = ["Yakshanba", "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba"];
+  day.innerHTML = weekDays[new Date().getDay()];
+
+  kun.innerHTML = new Date().getDate().toString().padStart(2, "0");
+
+  const months = [
+    "yanvar", "fevral", "mart", "aprel", "may", "iyun",
+    "iyul", "avgust", "sentabr", "oktabr", "noyabr", "dekabr"
+  ];
+  month.innerHTML = months[new Date().getMonth()];
+});
